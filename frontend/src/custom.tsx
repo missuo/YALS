07fed9776e5d1a961c53b8debe71b52b473ec341
Vincent Yang <missuo@pm.me>
2026-01-08 21:18:12 +0800

    fix: logo path

--- frontend/src/custom.tsx
+++ frontend/src/custom.tsx
@@ -10,7 +10,7 @@ export const config = {
   faviconPath: 'https://owo.network/favicon.ico',
   
   // Web top-left logo path, please put image files in public/images directory
-  logoPath: './assets/owo-logo.svg',
+  logoPath: 'owo-logo.svg',
   
   // Web background color
   backgroundColor: '#f5f5f5ff'
